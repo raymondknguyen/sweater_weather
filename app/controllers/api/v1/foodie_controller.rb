b12aec5e68737944e24aaa03e850c3bdfcbf9c2a
Raymond Nguyen <rayknguyen@gmail.com>
2020-06-08 09:09:36 -0700

    Add service

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -2,32 +2,38 @@ class Api::V1::FoodieController < ApplicationController
   def index
   start = params[:start] 
   des = params[:end]
-  search = [:search]
+  search = params[:search]
   
-    resp = Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |f|
-      f.params['key'] = ENV['GOOGLE_API_KEY']
-      f.params['origin'] = start
-      f.params['destination'] = des
-    end
-    x = JSON.parse(resp.body, symbolize_name: true)
-    travel_time = x['routes'][0]['legs'][0]['duration']['text']
-    end_location = x['routes'][0]['legs'][0]['end_address']
+    # resp = Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |f|
+    #   f.params['key'] = ENV['GOOGLE_API_KEY']
+    #   f.params['origin'] = start
+    #   f.params['destination'] = des
+    # end
+    # x = JSON.parse(resp.body, symbolize_name: true)
 
-    lat = x['routes'][0]['legs'][0]['end_location']['lat']
-    lng = x['routes'][0]['legs'][0]['end_location']['lng']
 
-    weather = WeatherService.new(lat, lng).weather_info
+    # travel_time = x['routes'][0]['legs'][0]['duration']['text']
+    # end_location = x['routes'][0]['legs'][0]['end_address']
+    # lat = x['routes'][0]['legs'][0]['end_location']['lat']
+    # lng = x['routes'][0]['legs'][0]['end_location']['lng']
+
+    location_info = GoogleDestinationService.new(start, des)
+    location_info.travel_time
+    location_info.end_location
+
+    weather = WeatherService.new(location_info.lat, location_info.lng).weather_info
     summary = weather[:current][:weather][0][:description]
     temp = weather[:current][:temp].to_i
 
     resp = Faraday.get('https://developers.zomato.com/api/v2.1/search') do |f|
       f.headers['user-key'] = ENV['ZOMATO_API_KEY']
-      f.params['lat'] = lat
-      f.params['lon'] = lng
-      f.params['cuisines'] = search
+      f.params['lat'] = location_info.lat
+      f.params['lon'] = location_info.lng
+      f.params['q'] = search
     end
     x = JSON.parse(resp.body, symbolize_name: true)
     name = x['restaurants'][0]['restaurant']['name']
     address = x['restaurants'][0]['restaurant']['location']['address']
+    binding.pry
   end
 end
---
+++ app/services/google_destination_service.rb
@@ -0,0 +1,34 @@
+class GoogleDestinationService
+  def initialize(start, des)
+    @start = start
+    @des = des
+  end 
+
+  def travel_time
+    resp['routes'][0]['legs'][0]['duration']['text']
+  end
+
+  def end_location
+    resp['routes'][0]['legs'][0]['end_address']
+  end
+
+  def lat 
+    resp['routes'][0]['legs'][0]['end_location']['lat']
+  end
+
+  def lng
+    resp['routes'][0]['legs'][0]['end_location']['lng']
+  end
+
+  def resp
+    JSON.parse(conn.body, symbolize_name: true)
+  end
+
+  def conn
+    Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |f|
+      f.params['key'] = ENV['GOOGLE_API_KEY']
+      f.params['origin'] = @start
+      f.params['destination'] = @des
+    end
+  end
+end
