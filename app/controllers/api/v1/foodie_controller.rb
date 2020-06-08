9867b8b5cfa1a985412ca2adc97f8fd1af3dda55
Raymond Nguyen <rayknguyen@gmail.com>
2020-06-08 08:56:59 -0700

    Everything works

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -20,12 +20,14 @@ class Api::V1::FoodieController < ApplicationController
     summary = weather[:current][:weather][0][:description]
     temp = weather[:current][:temp].to_i
 
-    # resp = Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |f|
-    #   f.params['key'] = ENV['GOOGLE_API_KEY']
-    #   f.params['origin'] = start
-    #   f.params['destination'] = des
-    # end
-
-    binding.pry
+    resp = Faraday.get('https://developers.zomato.com/api/v2.1/search') do |f|
+      f.headers['user-key'] = ENV['ZOMATO_API_KEY']
+      f.params['lat'] = lat
+      f.params['lon'] = lng
+      f.params['cuisines'] = search
+    end
+    x = JSON.parse(resp.body, symbolize_name: true)
+    name = x['restaurants'][0]['restaurant']['name']
+    address = x['restaurants'][0]['restaurant']['location']['address']
   end
 end
