a337f0c81591c5499a2164054da7dd61e17d7f53
Raymond Nguyen <rayknguyen@gmail.com>
2020-06-08 09:26:14 -0700

    add facade

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -4,17 +4,7 @@ class Api::V1::FoodieController < ApplicationController
   des = params[:end]
   search = params[:search]
 
-    location_info = GoogleDestinationService.new(start, des)
-    location_info.travel_time
-    location_info.end_location
-
-    weather = WeatherService.new(location_info.lat, location_info.lng).weather_info
-    summary = weather[:current][:weather][0][:description]
-    temp = weather[:current][:temp].to_i
-
-    restaurant_info = ZomatoService.new(location_info.lat, location_info.lng, search)
-    name = restaurant_info.name
-    address = restaurant_info.address
-    binding.pry
+  foodie = FoodieFacade.new(start, des, search)
+  
   end
 end
--- app/facades/foodie_facade.rb
+++ app/facades/foodie_facade.rb
@@ -1,6 +1,48 @@
 class FoodieFacade
-  attr_reader: :id
+  attr_reader :id
+
+  def initialize(start, des, search)
+    @start = start
+    @des = des 
+    @search = search 
+    @id = nil
+  end
 
   def travel_time
+    location_info.travel_time
+  end
+
+  def end_location
+    location_info.end_location
+  end
+
+  def summary
+    weather[:current][:weather][0][:description]
+  end
+  
+  def temp
+    weather[:current][:temp].to_i.to_s
+  end
+
+  def name
+    restaurant_info.name
+  end
+
+  def address
+    restaurant_info.address
+  end
+
+  private
+  
+  def restaurant_info
+     ZomatoService.new(location_info.lat, location_info.lng, @search)
+  end
+  
+  def weather
+    WeatherService.new(location_info.lat, location_info.lng).weather_info
+  end
+
+  def location_info
+    GoogleDestinationService.new(@start, @des)
   end
 end
