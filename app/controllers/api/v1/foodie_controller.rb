84c58894eea38a912e049d24a864fd74eb772578
Raymond Nguyen <rayknguyen@gmail.com>
2020-06-08 08:42:13 -0700

    Add weather

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -10,7 +10,22 @@ class Api::V1::FoodieController < ApplicationController
       f.params['destination'] = des
     end
     x = JSON.parse(resp.body, symbolize_name: true)
-    x['routes'][0]['legs'][0]['duration']['text']
+    travel_time = x['routes'][0]['legs'][0]['duration']['text']
+    end_location = x['routes'][0]['legs'][0]['end_address']
+
+    lat = x['routes'][0]['legs'][0]['end_location']['lat']
+    lng = x['routes'][0]['legs'][0]['end_location']['lng']
+
+    weather = WeatherService.new(lat, lng).weather_info
+    summary = weather[:current][:weather][0][:description]
+    temp = weather[:current][:temp].to_i
+
+    # resp = Faraday.get('https://maps.googleapis.com/maps/api/directions/json') do |f|
+    #   f.params['key'] = ENV['GOOGLE_API_KEY']
+    #   f.params['origin'] = start
+    #   f.params['destination'] = des
+    # end
+
     binding.pry
   end
 end
