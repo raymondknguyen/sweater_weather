91432073d7fe0cda20c83995ad4aa89cc0d4a7f8
Raymond Nguyen <rayknguyen@gmail.com>
2020-06-08 22:56:47 -0700

    Add road trip controller

--- spec/requests/api/v1/road_trip/user_can_request_road_trip_spec.rb
+++ spec/requests/api/v1/road_trip/user_can_request_road_trip_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "RoadTrip Request", :type => :request do 
   it 'can request roadtrip information' do
+    param = {
+    origin: "Denver,CO",
+    destination: "Pueblo,CO",
+    api_key: "jgn983hy48thw9begh98h4539h4"
+    }
+    post '/api/v1/road_trip', params: param
+
     
   end
 end
