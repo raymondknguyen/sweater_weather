feb1eefd876d7893dc1c369390047a77411db0ea
Raymond Nguyen <rayknguyen@gmail.com>
2020-06-08 22:46:26 -0700

    Add sessions request

---
+++ spec/requests/api/v1/road_trip/user_can_request_road_trip_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "RoadTrip Request", :type => :request do 
+  it 'can request roadtrip information' do
+    
+  end
+end
--- spec/requests/api/v1/user/user_can_request_login_spec.rb
+++ spec/requests/api/v1/user/user_can_request_login_spec.rb
@@ -3,16 +3,46 @@ require 'rails_helper'
 
 RSpec.describe "User Request", :type => :request do 
   it 'can log in' do
-    email = 'email@email.com'
-    password = 'password'
+    user_params = {
+      email: 'email@email.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }
+    User.create(user_params)
 
-    post "/api/v1/users?email=#{email}&password=#{password}&password_confirmation=#{password}"
+    user_params = {
+      email: 'email@email.com',
+      password: 'password',
+    }
+
+    post "/api/v1/sessions", params: user_params
 
     expect(response).to be_successful
     data = JSON.parse(response.body , symbolize_names: true)
     expect(data[:data][:type]).to eq("user")
     expect(data[:data][:attributes][:email]).to eq("email@email.com")
     expect(data[:data][:attributes][:api_key]).to_not be_nil
-    expect(response.status).to eq(201)
+    expect(response.status).to eq(200)
+  end
+
+  it 'cant log in' do
+    user_params = {
+      email: 'email@email.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }
+    User.create(user_params)
+
+    user_params = {
+      email: 'email@email.com',
+      password: '',
+    }
+
+    post "/api/v1/sessions", params: user_params
+
+    expect(response).to_not be_successful
+    data = JSON.parse(response.body , symbolize_names: true)
+    expect(data[:error]).to eq("Bad credentials. Please double check login request.")
+    expect(response.status).to eq(400)
   end
 end
